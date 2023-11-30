00918dba2f91b0b0d076b881a883462f51c87a87
0obito <elmsafer.abdellah00@gmail.com>
2023-11-30 22:10:34 +0100

    555

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     import variable_load_5
-    for i in dir(variable_load_5):
-        if i[:1] == "a":
-            print(i[4:6])
+    print("{}".format(variable_load_5.a))
