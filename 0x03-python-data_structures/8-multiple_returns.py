208abb0ef9aeff5aefa734be5fb768678e45f2bc
0obito <elmsafer.abdellah00@gmail.com>
2023-12-04 19:03:16 +0100

    ansja

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return (0, '')
+        return (0, "")
     else:
         result = (len(sentence), sentence[0])
         return result
