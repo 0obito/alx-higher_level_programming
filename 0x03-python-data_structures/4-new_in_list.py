fd69fca8347abd13c04484d3f4701dba4a43f3d3
0obito <elmsafer.abdellah00@gmail.com>
2023-12-04 17:07:18 +0100

    pycodestyle

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,7 +5,7 @@ def new_in_list(my_list, idx, element):
     for var in my_list:
         copy.append(var)
     if idx < 0 or idx >= len(my_list):
-       return copy
+        return copy
     else:
         copy[idx] = element
         return copy
