40518c196cd96d67cbdba29f6c9e287a4f1b8f7e
0obito <elmsafer.abdellah00@gmail.com>
2023-12-19 20:48:51 +0100

    Edit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """A simple class representing a square.
-
-    This class is a basic representation of a square.
-
-    """
-    def __init__(self):
-        """Initialize a square with self
-
-        """
-        pass
+    """Empty class defining a square"""
+    pass
