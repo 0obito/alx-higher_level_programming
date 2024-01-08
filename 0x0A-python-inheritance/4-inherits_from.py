2847103832d91dba4e4b2fb654c24964b6f3561c
0obito <elmsafer.abdellah00@gmail.com>
2024-01-08 19:29:20 +0100

    Task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for inherits_from.py"""
+
+
+def inherits_from(obj, a_class):
+    """Function that checks if an object is an instance of a sub-class
+       of the given class.
+    """
+    return issubclass(obj, a_class)
