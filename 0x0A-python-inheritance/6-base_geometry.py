25bf8c84262855516a8b7a17feb72ad4126bbe5a
0obito <elmsafer.abdellah00@gmail.com>
2024-01-08 19:46:00 +0100

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for 5-base_geometry.py"""
+
+
+class BaseGeometry:
+    """an empty class BaseGeometry"""
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
