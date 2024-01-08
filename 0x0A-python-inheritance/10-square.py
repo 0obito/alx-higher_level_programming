2a37ca72333d6476309aa5f45f52c451ea3ec15f
0obito <elmsafer.abdellah00@gmail.com>
2024-01-08 20:56:00 +0100

    Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for 10-square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class that inherits from Rectange class"""
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = size
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+
+    def area(self):
+        """area method implementation"""
+        return super().area()
