048bfc14cb1043417dd866d388ce7cb193ade8da
0obito <elmsafer.abdellah00@gmail.com>
2024-01-09 18:02:38 +0100

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """7-add_item module"""
 import sys
-import json
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
 
 
-try:
-    mylist = load('add_item.json')
-except (FileNotFoundError, json.JSONDecodeError):
-    mylist = []
+if __name__ == "__main__":
 
-for item in sys.argv[1:]:
-    mylist.append(item)
+    save = __import__('5-save_to_json_file').save_to_json_file
+    load = __import__('6-load_from_json_file').load_from_json_file
 
-save(mylist, 'add_item.json')
+    try:
+        mylist = load('add_item.json')
+    except FileNotFoundError:
+        mylist = []
+
+    for item in sys.argv[1:]:
+        mylist.append(item)
+
+    save(mylist, 'add_item.json')
