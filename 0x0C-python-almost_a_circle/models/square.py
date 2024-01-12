1d1019a772536fa6525711570cfa6fc539565f7e
0obito <elmsafer.abdellah00@gmail.com>
2024-01-12 20:00:05 +0100

    Square size

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Square module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialization"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """The overloading method"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """size setter"""
+        if not isinstance(size, int):
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
+        self.width = size
+        self.height = size
