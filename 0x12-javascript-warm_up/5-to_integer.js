57a730f5c794815a526d1c46f8d89ac1f05a24d6
0obito <elmsafer.abdellah00@gmail.com>
2024-04-08 23:15:46 +0000

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-if (process.argv[2]) {
-  if (!isNaN(parseInt(process.argv[2]))) {
-    console.log('My number: ', parseInt(process.argv[2]));
-  } else {
-    console.log('Not a number');
-  }
+const argument = process.argv[2];
+const num = parseInt(argument);
+
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
 } else {
-  console.log('Not a number');
+  console.log("Not a number");
 }
