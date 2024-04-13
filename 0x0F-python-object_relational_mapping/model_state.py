7951f1a0404067a2668a1d150dac6f972156a290
0obito <elmsafer.abdellah00@gmail.com>
2024-04-13 20:01:47 +0000

    more updates

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,13 +5,18 @@ and an instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
+
 Base = declarative_base()
 
 
 class State(Base):
+    """
+    State class inherits the Base class
+        Attributes:
+            id (int)
+            name (string)
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
