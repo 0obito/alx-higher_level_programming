fe23275144365ead073516fbd3b0272e36629d9a
0obito <elmsafer.abdellah00@gmail.com>
2024-04-13 10:28:43 +0000

    added the first python script

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+   This is select_states module, it lists
+   all states from a chosen database
+"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = connection.cursor()
+    sql_query = "SELECT * FROM states"
+    cursor.execute(sql_query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    connection.close()
+    
