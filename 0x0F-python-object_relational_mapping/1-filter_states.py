0970cb195c75cad9bea926012da41f426935baff
0obito <elmsafer.abdellah00@gmail.com>
2024-04-13 13:08:47 +0000

    added order sorting to file 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,13 +5,13 @@ starting with N from the given database
 """
 import MySQLdb
 import sys
-
 if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost",
                                  port=3306, user=sys.argv[1],
-                                 passwd=sys.argv[2], db=sys.argv[3])
+                                 passwd=sys.argv[2],
+                                 db=sys.argv[3])
     cursor = connection.cursor()
-    sql_query = "SELECT * FROM states WHERE name LIKE 'N%'"
+    sql_query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
     cursor.execute(sql_query)
     many = cursor.fetchall()
     for one in many:
